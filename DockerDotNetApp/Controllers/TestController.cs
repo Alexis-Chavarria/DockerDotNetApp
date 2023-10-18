c5064e44fd35190119e302e5e02a716a0879f22f
Johan-Chavarria <johan_chavarria@cun.edu.co>
2023-10-18 14:45:32 -0500

    Se inicia proyecto con un controlador de prueba "Test"

---
+++ DockerDotNetApp/Controllers/TestController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DockerDotNetApp.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
